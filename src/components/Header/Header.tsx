418d07d306ccf75e664323c88a2467d80c7affcb
Jonas <joda12@elev.techcollege.dk>
2026-09-08 15:00:07 +0200

    First commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import { HeaderStyled } from "./Header.styled"
+
+export const Header = () => {
+  return (
+    <HeaderStyled>
+      <div className="titleContainer">
+        <h1>Test</h1>
+        <p>Hej med dig</p>
+      </div>
+    </HeaderStyled>
+  )
+}
